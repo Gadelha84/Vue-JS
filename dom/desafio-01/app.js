d0e5208357c68a6eca0f78b16eb35d7570fb5234
Paulo Gadelha <pc.gadelha@hotmail.com>
2022-10-19 20:35:36 -0300

    exercise commit
    commit com exercício 1 do estudo

--- dom/desafio-01/app.js
+++ dom/desafio-01/app.js
@@ -3,12 +3,15 @@ new Vue({
     data: {
         name: 'Paulo Gadelha',
         age: 38,
-        linkHtml: '<a href="http://google.com.br">Google</a>'
+        imgLink: 'https://blog.logrocket.com/wp-content/uploads/2021/09/ultimate-guide-SVGs-vue-js.png'
     },
 
     methods: {
         randomNumber: function() {
             return Math.random(1)
+        },
+        alterarNome(event) {
+            this.name = event.target.value
         }
     }
 })
