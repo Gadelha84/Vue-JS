e3a33072aa3fb72bafddf8469c9fe387dd3a0f41
Paulo Gadelha <pc.gadelha@hotmail.com>
2022-11-30 20:28:19 -0300

    Components commit
    Adição de componentes e registros locais e globais

---
+++ componentes/componente-intro/src/AppContadores.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="c in 5" :key="c"/>
+    </div>
+</template>
+
+<script>
+import Contador from './AppContador.vue'
+export default {
+    components: {
+        'app-contador': Contador
+    }
+}
+</script>
+
+<style>
+    
+</style>
