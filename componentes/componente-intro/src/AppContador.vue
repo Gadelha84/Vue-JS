c0a3bc0bd08bb1d06ae5e81e1b2cc59aeedd5aaf
Paulo Gadelha <pc.gadelha@hotmail.com>
2022-12-06 12:36:45 -0300

    components commit
    Adição de componentes em Vue

--- componentes/componente-intro/src/AppContador.vue
+++ componentes/componente-intro/src/AppContador.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="contador">
         <span>{{ contador }}</span>
-        <button @click="adicionar">Somar</button>
-        <button @click="subtrair">Subtrair</button>
+        <button @click="adicionar">+</button>
+        <button @click="subtrair">-</button>
     </div>
 </template>
 
@@ -24,14 +24,14 @@ export default {
 }
 </script>
 
-<style>
-    .contador span {
+<style scoped>
+    span {
         border-bottom: 1px solid #ccc;
         height: 30px;
         padding: 5px 25px;
     }
 
-    .contador button {
+    button {
         height: 30px;
         widows: 30px;
         border-radius: 15px;
